b7bea480784a0198682afde33ae7b9ee95c8e9cc
Davicesar01 <147111149+Davicesar01@users.noreply.github.com>
2024-07-02 21:36:24 -0300

    atividade 02/07/2024

---
+++ input&printf 02.07.24.py
@@ -0,0 +1,17 @@
+# Boa noite, segue nosso primeiro programa de forma autonomo.
+#print('\n Olá, qual é o seu Nome:') 
+#print que serve para exibir a tela e temos o input
+#name = input ('Pode digitar o seu nome? ') 
+#input, serve para inserir texto via console. 
+
+#print('Olá',name,  'seja bem vindo ao nosso App')
+
+
+## Faça um aplicativo aonde recebe o nome da pessoa, email CPF e CEP
+
+nome= input ('Digite seu nome:')
+cpf= input ('Qual é o seu cpf:')
+email=input ('Diga qual é o seu e-mail:')
+cep= input ('Qual e o seu endereço:')
+
+print('Seja bem vindo',nome,' seu endereço é',cep,', sua conta de e-mail é',email,'seu status no serasa é,',cpf)
